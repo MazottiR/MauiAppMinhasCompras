55677e0c17f8042d40c8f9f2021759810a0bb6ae
Usuario <Usuario@CPD03-M13>
2024-11-28 14:59:18 -0300

    Finalizado

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -10,7 +10,7 @@ public partial class ListaProduto : ContentPage
 	{
 		InitializeComponent();
 
-		lst_produtos.ItemSource = lista;
+		lst_produtos.ItemsSource = lista;
 	}
 
     protected async override void OnAppearing()
@@ -96,6 +96,23 @@ public partial class ListaProduto : ContentPage
         }
 		catch (Exception ex)
 		{
+            DisplayAlert("Ops", ex.Message, "Ok");
+        }
+    }
+
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+		try
+		{
+			Produto p = e.SelectedItem as Produto;
+
+			Navigation.PushAsync(new Views.EditarProduto
+			{
+				BindingContext = p,
+			});
+		}
+        catch (Exception ex)
+        {
             DisplayAlert("Ops", ex.Message, "Ok");
         }
     }
